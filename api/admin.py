739fd0ecdbd396c4eaeb7a7dc6c91739bee5ab79
Keybeen <keybeen.videoeditor@gmail.com>
2026-04-17 14:33:01 +0800

    feat: register models in Django admin
    Register all models for admin interface:
    - Coin
    - Trade
    - EmotionTag
    - TradeEmotion
    - PortfolioSnapshot
    - MonthlyReport
    - AIFeedback
    
    Enables CRUD operations for admin users.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Coin, Trade, EmotionTag, TradeEmotion, PortfolioSnapshot, MonthlyReport, AIFeedback
+
 
 # Register your models here.
+admin.site.register(Coin)
+admin.site.register(Trade)
+admin.site.register(EmotionTag)
+admin.site.register(TradeEmotion)
+admin.site.register(PortfolioSnapshot)
+admin.site.register(MonthlyReport)
+admin.site.register(AIFeedback)
